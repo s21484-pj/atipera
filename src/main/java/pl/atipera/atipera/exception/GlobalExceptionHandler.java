ef9966d0e8b02735980e3fc3ada48e200b0fc964
s21484-pj <s21484@pjwstk.edu.pl>
2024-02-22 01:21:06 +0100

    Add exception handling

---
+++ src/main/java/pl/atipera/atipera/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package pl.atipera.atipera.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.RestClientException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RestClientException.class)
+    public ResponseEntity<ApiError> handleEntityNotFoundException(RestClientException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiError(HttpStatus.NOT_FOUND, exception.getMessage()));
+    }
+}
